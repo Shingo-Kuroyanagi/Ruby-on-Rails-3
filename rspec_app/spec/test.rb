5480a252cbf932b1d0ff7d7a589690bbb5a519ac
EC2 Default User <ec2-user@ip-172-31-13-10.ec2.internal>
2020-09-29 13:26:45 +0000

    Rails 開発スキルアップト

---
+++ rspec_app/spec/test.rb
@@ -0,0 +1,13 @@
+describe '四則計算' do
+  context '足し算' do
+    it '1+1は２になる' do
+      expect(1 + 1).to eq 2
+    end
+  end
+
+  context '引き算' do
+    it'1+1は２になる' do
+      expect(1 + 1).to eq 3
+    end
+  end
+end
